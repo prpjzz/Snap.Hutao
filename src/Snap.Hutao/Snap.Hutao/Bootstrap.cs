c167db3ac044f9b2c725a7a62bc42761e276d827
prpjzz <groovemusic4399@pm.me>
2025-04-15 10:19:51 +0700

    minor patch

--- src/Snap.Hutao/Snap.Hutao/Bootstrap.cs
+++ src/Snap.Hutao/Snap.Hutao/Bootstrap.cs
@@ -113,11 +113,11 @@ public static partial class Bootstrap
 
     private static bool OSPlatformSupported()
     {
-        if (new UniversalApiContract.Version("10", "0", "19045", "5371") > UniversalApiContract.WindowsVersion)
+        if (new UniversalApiContract.Version("10", "0", "19044", "5371") > UniversalApiContract.WindowsVersion)
         {
             const string Message = """
-                Snap Hutao 无法在版本低于 10.0.19045.5371 的 Windows 上运行，请更新系统。
-                Snap Hutao cannot run on Windows versions earlier than 10.0.19045.5371. Please update your system.
+                Snap Hutao 无法在版本低于 10.0.19044.5371 的 Windows 上运行，请更新系统。
+                Snap Hutao cannot run on Windows versions earlier than 10.0.19044.5371. Please update your system.
                 """;
             User32.MessageBoxExW(
                 default,
